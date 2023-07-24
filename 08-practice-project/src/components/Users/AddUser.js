29eb7960e62fb19a534a55237b6ab27adc9664cf
yui880 <kimyuna521@gmail.com>
2023-07-24 23:40:41 +0900

    feat : 09-fragemts-portals-refs 코드 작성 완료

--- 08-practice-project/src/components/Users/AddUser.js
+++ 08-practice-project/src/components/Users/AddUser.js
@@ -1,48 +1,61 @@
+import React, { useRef } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import { useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUsername, setEnteredUsername] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState(undefined);
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    const username = nameInputRef.current.value;
+    const age = ageInputRef.current.value;
+
+    if (username.trim().length === 0 || age.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values)",
       });
       return;
     }
-    if (parseInt(enteredAge) < 1) {
+    if (parseInt(age) < 1) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid age (>0)",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    props.onAddUser(username, age);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+
+    // setEnteredUsername("");
+    // setEnteredAge("");
   };
 
-  const userNameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const userNameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
+  //
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
   };
 
   return (
-    <>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -56,20 +69,22 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={userNameChangeHandler}
+            // value={enteredUsername}
+            // onChange={userNameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </>
+    </React.Fragment>
   );
 };
 
