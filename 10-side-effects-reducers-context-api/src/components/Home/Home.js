08cce8183c4c33f5262a9d619b8127d457f0ee5e
yui880 <kimyuna521@gmail.com>
2023-07-27 23:10:16 +0900

    feat : 10-side-effects-reducers-context-api Context API 코드 작성

--- 10-side-effects-reducers-context-api/src/components/Home/Home.js
+++ 10-side-effects-reducers-context-api/src/components/Home/Home.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Home.module.css';
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+import AuthContext from "../../store/auth-context";
+import Button from "../UI/Button/Button";
 
 const Home = (props) => {
+  const AuthCtx = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={AuthCtx.onLogout}>LogOut!</Button>
     </Card>
   );
 };
--- 10-side-effects-reducers-context-api/src/components/Login/Login.js
+++ 10-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -1,8 +1,17 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
+import authContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -34,6 +43,8 @@ const Login = (props) => {
     isValid: undefined,
   });
 
+  const AuthCtx = useContext(AuthContext);
+
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
   // const [enteredPassword, setEnteredPassword] = useState("");
@@ -42,6 +53,9 @@ const Login = (props) => {
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     const identifier = setTimeout(() => {
@@ -77,42 +91,40 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      AuthCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          ref={emailInputRef}
+          id="email"
+          type="email"
+          label="E-Mail"
+          value={emailState.value}
+          isValid={emailIsValid}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          ref={passwordInputRef}
+          id="password"
+          type="password"
+          label="Password"
+          value={passwordState.value}
+          isValid={passwordIsValid}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} ㅜ>
             Login
           </Button>
         </div>
